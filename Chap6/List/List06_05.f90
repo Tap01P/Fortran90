8b79f5afb860148896163681f1cde27a2ac0b0bc
Tap01P <tap01p.numerical@gmail.com>
2018-01-24 20:55:52 +0900

    リスト6.5の修正及びグラフ出力部分の実装 ・y = 0における境界条件の設定に不備があったので修正 ・gnuplotを呼び出して3次元プロットを行なった（向きがよろしくないが）

--- Chap6/List/List06_05.f90
+++ Chap6/List/List06_05.f90
@@ -3,11 +3,11 @@
 ! このリストではひとまず全ての処理をメインプログラム内に記述する
 program main
     implicit none
-    integer i, j, k, itr
-    integer, parameter :: n1 = 31, n2 = 31, itrmax = 100
+    integer i, j, itr
+    integer, parameter :: n1 = 101, n2 = 101, itrmax = 100
     real(8) x(2, n1, n2), phi(n1, n2)
     real(8) dx1, dx2, rhs, er, c, d, omega
-    real(8) :: pi = 2.0d0 * acos(0.0d0) ,er0 = 10e-8
+    real(8) :: pi = 2.0d0 * acos(0.0d0) ,er0 = 10E-6
     omega = 2.0d0 / (1.0d0 + sin(pi/dble(n1 - 1)) )
     dx1 = 1.0d0 / dble(n1 - 1)
     dx2 = 1.0d0 / dble(n2 - 1)
@@ -16,18 +16,28 @@ program main
     d = - ( dx1 * dx1 ) /( 2.0d0 * ( dx1 * dx1 + dx2 * dx2 ) )
     ! 格子点の設定
     do i = 1, n1
-        x(1, i, 1:n2) = dble(i - 1) * dx1
+        do j = 1, n2
+            x(1, i, j) = dble(i - 1) * dx1
+        end do
     end do
-    do i = 1, n2
-        x(2, 1:n1, i) = dble(i - 1) * dx2
+    do j = 1, n2
+        do i = 1, n1
+            x(2, i, j) = dble(j - 1) * dx2
+        end do
     end do
     ! ディレクレ境界条件の設定
-    phi = 1.0d0
-    phi(1, 1:n2) = 0.0d0
-    phi(n1, 1:n2) = 0.0d0
-    phi(1:n1, n2) = 0.0d0
+    phi = 0.2d0
+    do j = 1, n2
+        phi(1, j) = 0.0d0
+    end do
+    do j = 1, n2
+        phi(n1, j) = 0.0d0
+    end do
     do i = 1, n1
-        phi(i, 1) = sin(pi * i)
+        phi(i, n2) = 0.0d0
+    end do
+    do i = 1, n1
+        phi(i, 1) = sin(pi * x(1, i, 1) )
     end do
     ! SOR法の反復計算
     do itr = 1, itrmax
@@ -43,4 +53,20 @@ program main
         write(*, *) 'itr, er =', itr, er
         if (er < er0) exit
     end do
+    open(10, file = 'LaplaceNu.dat')
+    do j = 1, n2
+        do i = 1, n1
+            write(10, '(3e12.4)') x(1, i, j), x(2, i, j), phi(i, j)
+        end do
+            write(10, *) ''
+    end do
+    close(10)
+    open(11, file = 'plotNsol.plt')
+    write(11, *) 'reset'
+    write(11, *) 'set pm3d'
+    write(11, *) 'set palette rgbformulae 33, 13, 10'
+    write(11, *) 'splot "LaplaceNu.dat" with pm3d'
+    write(11, *) 'pause -1'
+    close(11)
+    call system('gnuplot plotNsol.plt')
 end program main
